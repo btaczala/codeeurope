bbb244728dffbc714b6205cb9f043727a239f004
Bartek Taczała <bartek.taczala@gmail.com>
2016-07-19 09:44:49 +0200

    Remove functions 98

--- common.hpp
+++ common.hpp
@@ -21,10 +21,6 @@ struct remove_pointer<T *const volatile> {
     typedef T type;
 };
 
-std::string freeFunction(const std::string &,
-                         const std::vector<std::string> &vec) {
-    return "";
-}
 
 struct SomeType {
     std::string aFunction(const std::string &,
--- functions.cpp
+++ functions.cpp
@@ -4,22 +4,51 @@
 #include <map>
 #include "common.hpp"
 
-typedef std::function<std::string(const std::string &,
-                                  const std::vector<std::string> &)> Callback_t;
+std::string freeFunction(const std::string &,
+                         const std::vector<std::string> &vec) {
+    return "";
+}
+
+typedef std::function<std::string(
+    const std::string &, const std::vector<std::string> &)> StdCallback_t;
+// function pointers
+struct SomeType;
+typedef std::string (SomeType::*Callback_t)(const std::string &,
+                                            const std::vector<std::string> &);
+typedef std::string (*FreeCallback_t)(const std::string &,
+                                      const std::vector<std::string> &);
 
 struct DifferentType {
-    std::map<std::string, std::string> anotherFunction(const Callback_t &t) {
+    std::map<std::string, std::string> anotherFunction(const StdCallback_t &t) {
         std::map<std::string, std::string> ret;
         ret[t("", std::vector<std::string>())];
         return ret;
     }
+
+    std::map<std::string, std::string> anotherFunction(Callback_t &t,
+                                                       SomeType &type) {
+        std::map<std::string, std::string> ret;
+        ret[((type).*(t))("", std::vector<std::string>())];
+        return ret;
+    }
 };
 
-void freeFunctionTest(benchmark::State &state) {
+void oldStylefreeFunctionTest(benchmark::State &state) {
     while (state.KeepRunning()) {
-        Callback_t cb{freeFunction};
+        FreeCallback_t cb = &freeFunction;
         for (int i = 0; i < state.range_x(); ++i) {
-            cb("", std::vector<std::string>());
+            auto result = cb("", std::vector<std::string>());
+            benchmark::DoNotOptimize(result);
+        }
+    }
+}
+
+void newStylefreeFunctionTest(benchmark::State &state) {
+    while (state.KeepRunning()) {
+        StdCallback_t cb{&freeFunction};
+        for (int i = 0; i < state.range_x(); ++i) {
+            auto result = cb("", std::vector<std::string>());
+            benchmark::DoNotOptimize(result);
         }
     }
 }
@@ -27,7 +56,7 @@ void freeFunctionTest(benchmark::State &state) {
 void memberFunctionTest(benchmark::State &state) {
     SomeType t;
     while (state.KeepRunning()) {
-        Callback_t cb;
+        StdCallback_t cb;
         for (int i = 0; i < state.range_x(); ++i) {
             cb = std::bind(&SomeType::aFunction, &t, std::placeholders::_1,
                            std::placeholders::_2);
@@ -49,7 +78,8 @@ void indirectCall(benchmark::State &state) {
     }
 }
 
-BENCHMARK(freeFunctionTest)->Range(8, 8 << 10);
-BENCHMARK(memberFunctionTest)->Range(8, 8 << 10);
-BENCHMARK(indirectCall)->Range(8, 8 << 10);
+BENCHMARK(oldStylefreeFunctionTest)->Range(8, 8 << 10);
+BENCHMARK(newStylefreeFunctionTest)->Range(8, 8 << 10);
+// BENCHMARK(memberFunctionTest)->Range(8, 8 << 10);
+// BENCHMARK(indirectCall)->Range(8, 8 << 10);
 BENCHMARK_MAIN()
